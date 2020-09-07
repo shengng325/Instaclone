67625c40b8d206d6ed7016eb0e9b363318a1a548
shengng325 <shengng325@gmail.com>
2020-09-07 23:21:59 +0800

    Add FAQ and 404 page

--- main.go
+++ main.go
@@ -1,8 +1,38 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1> Home. Welcome! </h1>")
+}
+
+func ContactsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1> Contact page </h1>")
+}
+
+func FaqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1> FAQ </h1>")
+}
+
+func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1> 404 NOT FOUND!!! </h1>")
+}
 
 func main() {
-	println("Hello, World!")
-	http.ListenAndServe(":3000", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/contacts", ContactsHandler)
+	r.HandleFunc("/faq", FaqHandler)
+	r.NotFoundHandler = http.HandlerFunc(NotFoundHandler)
+	http.ListenAndServe(":3000", r)
 }
