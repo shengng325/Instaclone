9431236a4f88762ae6441d2da2a11c08d804af96
shengng325 <shengng325@gmail.com>
2020-09-20 22:54:05 +0800

    add controller package

---
+++ controllers/users.go
@@ -0,0 +1,25 @@
+package controllers
+
+import (
+	"net/http"
+
+	"lenslocked.com/views"
+)
+
+//init user
+func InitUser() *User {
+	return &User{
+		NewView: views.NewView("bootstrap", "views/users/new.gohtml"),
+	}
+}
+
+type User struct {
+	NewView *views.View
+}
+
+func (u *User) Handler(w http.ResponseWriter, r *http.Request) {
+	err := u.NewView.Render(w, nil)
+	if err != nil {
+		panic(err)
+	}
+}
--- main.go
+++ main.go
@@ -2,31 +2,26 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
-	"github.com/julienschmidt/httprouter"
+	"github.com/gorilla/mux"
+
+	"lenslocked.com/controllers"
 	"lenslocked.com/views"
 )
 
 var homeView *views.View
 var contactView *views.View
-var signupView *views.View
 
-func HomeHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	must(homeView.Render(w, nil))
 
 }
 
-func ContactsHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+func ContactsHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	must(contactView.Render(w, nil))
-}
-
-func SignupHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	w.Header().Set("Content-Type", "text/html")
-	must(signupView.Render(w, nil))
 }
 
 func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
@@ -38,15 +33,14 @@ func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
-	signupView = views.NewView("bootstrap", "views/signup.gohtml")
+	usersC := controllers.InitUser()
 
-	router := httprouter.New()
-	router.GET("/", HomeHandler)
-	router.GET("/contact", ContactsHandler)
-	router.GET("/signup", SignupHandler)
-	router.NotFound = http.HandlerFunc(NotFoundHandler)
-
-	log.Fatal(http.ListenAndServe(":3000", router))
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/contact", ContactsHandler)
+	r.HandleFunc("/signup", usersC.Handler)
+	r.NotFoundHandler = http.HandlerFunc(NotFoundHandler)
+	http.ListenAndServe(":3000", r)
 }
 
 func must(err error) {
