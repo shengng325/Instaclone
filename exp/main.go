242551e7a2341dd6acfea439d76bea79317c8cd1
shengng325 <shengng325@gmail.com>
2020-09-22 17:49:45 +0800

    dd

--- exp/main.go
+++ exp/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 
-	_ "github.com/lib/pq"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 const (
@@ -19,78 +19,10 @@ func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
+	db, err := gorm.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
 
-	// var id int
-	// var name, email string
-	// row := db.QueryRow(`
-	// 	INSERT INTO users(name, email)
-	// 	VALUES($1, $2)
-	// 	RETURNING id`,
-	// 	"Jons Calhoun", "jons@calhoun.io")
-	// err = row.Scan(&id)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Println("return id...", id)
-
-	// row := db.QueryRow(`
-	// 	SELECT id, name, email
-	// 	FROM users
-	// 	WHERE id=$1`, 1)
-	// err = row.Scan(&id, &name, &email)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Println("return id...", id, "...name...", name, "...email...", email)
-
-	// type User struct {
-	// 	id    int
-	// 	name  string
-	// 	email string
-	// }
-	// var users []User
-	// rows, err := db.Query(`
-	// 	SELECT id, name, email
-	// 	FROM users`)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// defer rows.Close()
-	// for rows.Next() {
-	// 	var user User
-	// 	err = rows.Scan(&user.id, &user.name, &user.email)
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	// 	users = append(users, user)
-	// }
-	// if rows.Err() != nil {
-	// 	panic(err)
-	// }
-	// fmt.Println(users)
-
-	var id int
-	for i := 1; i < 6; i++ {
-		// Create some fake data
-		userId := 1
-		if i > 3 {
-			userId = 2
-		}
-		amount := 1000 * i
-		description := fmt.Sprintf("USB-C Adapter x%d", i)
-		_, err = db.Exec(`
-			INSERT INTO orders (user_id, amount, description)
-			VALUES($1, $2, $3)`,
-			userId, amount, description)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println("Created an order with the ID:", id)
-	}
-
-	db.Close()
+	defer db.Close()
 }
