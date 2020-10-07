7711a32bdcf4dc3f78a3ae1dbe024c64ff4324ce
shengng325 <shengng325@gmail.com>
2020-10-07 21:45:10 +0800

    added edit page

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -20,6 +20,8 @@ func NewGalleries(gs models.GalleryService, r *mux.Router) *Galleries {
 	return &Galleries{
 		NewGallery: views.NewView("bootstrap", "galleries/new"),
 		ShowView:   views.NewView("bootstrap", "galleries/show"),
+		EditView:   views.NewView("bootstrap", "galleries/edit"),
+		IndexView:  views.NewView("bootstrap", "galleries/index"),
 		gs:         gs,
 		r:          r,
 	}
@@ -28,6 +30,8 @@ func NewGalleries(gs models.GalleryService, r *mux.Router) *Galleries {
 type Galleries struct {
 	NewGallery *views.View
 	ShowView   *views.View
+	EditView   *views.View
+	IndexView  *views.View
 	gs         models.GalleryService
 	r          *mux.Router
 }
@@ -37,27 +41,31 @@ type GalleryForm struct {
 }
 
 func (g *Galleries) Show(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	idStr := vars["id"]
-	id, err := strconv.Atoi(idStr)
+	gallery, err := g.galleryByID(w, r)
 	if err != nil {
-		http.Error(w, "Invalid gallery ID", http.StatusNotFound)
-		return
-	}
-	gallery, err := g.gs.ByID(uint(id))
-	if err != nil {
-		switch err {
-		case models.ErrNotFound:
-			http.Error(w, "Gallery not found", http.StatusNotFound)
-		default:
-			http.Error(w, "Whoops! Something went wrong.", http.StatusInternalServerError)
-		}
 		return
 	}
 
 	var vd views.Data
 	vd.Yield = gallery
 	g.ShowView.Render(w, vd)
+}
+
+// GET /galleries/:id/edit
+func (g *Galleries) Edit(w http.ResponseWriter, r *http.Request) {
+	gallery, err := g.galleryByID(w, r)
+	if err != nil {
+		return
+	}
+	user := context.User(r.Context())
+	if gallery.UserID != user.ID {
+		// http.Error(w, "You do not have permission to edit this gallery", http.StatusForbidden)
+		http.Error(w, "Gallery not found", http.StatusNotFound)
+		return
+	}
+	var vd views.Data
+	vd.Yield = gallery
+	g.EditView.Render(w, vd)
 }
 
 // POST /galleries
@@ -93,4 +101,33 @@ func (g *Galleries) Create(w http.ResponseWriter, r *http.Request) {
 	}
 	http.Redirect(w, r, url.Path, http.StatusFound)
 
+}
+
+// galleryByID will parse the "id" variable from the
+// request path using gorilla/mux and then use that ID to
+// retrieve the gallery from the GalleryService
+//
+// galleryByID will return an error if one occurs, but it
+// will also render the error with an http.Error function
+// call, so you do not need to.
+func (g *Galleries) galleryByID(w http.ResponseWriter,
+	r *http.Request) (*models.Gallery, error) {
+	vars := mux.Vars(r)
+	idStr := vars["id"]
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid gallery ID", http.StatusNotFound)
+		return nil, err
+	}
+	gallery, err := g.gs.ByID(uint(id))
+	if err != nil {
+		switch err {
+		case models.ErrNotFound:
+			http.Error(w, "Gallery not found", http.StatusNotFound)
+		default:
+			http.Error(w, "Whoops! Something went wrong.", http.StatusInternalServerError)
+		}
+		return nil, err
+	}
+	return gallery, nil
 }
--- main.go
+++ main.go
@@ -70,6 +70,9 @@ func main() {
 	r.Handle("/galleries/new", requireUserMw.Apply(galleriesC.NewGallery)).Methods("GET")
 	r.HandleFunc("/galleries", requireUserMw.ApplyFn(galleriesC.Create)).Methods("POST")
 	r.HandleFunc("/galleries/{id:[0-9]+}", galleriesC.Show).Methods("GET").Name(controllers.ShowGallery)
+	r.HandleFunc("/galleries/{id:[0-9]+}/edit", requireUserMw.ApplyFn(galleriesC.Edit)).Methods("GET")
+	// r.HandleFunc("/galleries/{id:[0-9]+}/update", requireUserMw.ApplyFn(galleriesC.Update)).Methods("POST")
+	// r.HandleFunc("/galleries/{id:[0-9]+}/delete", requireUserMw.ApplyFn(galleriesC.Delete)).Methods("POST")
 
 	fmt.Println("Server running at :3000")
 	http.ListenAndServe(":3000", r)
