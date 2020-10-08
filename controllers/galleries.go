b1935056e99e7781fd92bcaff28a7da218c92311
shengng325 <shengng325@gmail.com>
2020-10-08 22:14:49 +0800

    halfway commit

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -2,7 +2,10 @@ package controllers
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
 	"strconv"
 
 	"github.com/gorilla/mux"
@@ -14,6 +17,10 @@ import (
 const (
 	ShowGallery = "show_gallery"
 	EditGallery = "edit_gallery"
+	EditGallery = "edit_gallery"
+
+	maxMultipartMem = 1 << 20 // 1 megabyte
+
 )
 
 //init user
@@ -110,6 +117,74 @@ func (g *Galleries) Update(w http.ResponseWriter, r *http.Request) {
 			Level:   views.LevelSuccess,
 			Message: "Gallery successfully updated!",
 		}
+	}
+	g.EditView.Render(w, r, vd)
+}
+
+// POST /galleries/:id/images
+func (g *Galleries) ImageUpload(w http.ResponseWriter, r *http.Request) {
+	gallery, err := g.galleryByID(w, r)
+	if err != nil {
+		return
+	}
+	user := context.User(r.Context())
+	if gallery.UserID != user.ID {
+		http.Error(w, "Gallery not found", http.StatusNotFound)
+		return
+	}
+
+	var vd views.Data
+	vd.Yield = gallery
+	err = r.ParseMultipartForm(maxMultipartMem)
+	if err != nil {
+		vd.SetAlert(err)
+		g.EditView.Render(w, r, vd)
+		return
+	}
+
+	// Create the directory to contain our images
+	galleryPath := filepath.Join("images", "galleries",
+		fmt.Sprintf("%v", gallery.ID))
+	err = os.MkdirAll(galleryPath, 0755)
+	if err != nil {
+		vd.SetAlert(err)
+		g.EditView.Render(w, r, vd)
+		return
+	}
+
+	// Iterate over uploaded files to process them.
+	files := r.MultipartForm.File["images"]
+	for _, f := range files {
+		// Open the uploaded file
+		file, err := f.Open()
+		if err != nil {
+			vd.SetAlert(err)
+			g.EditView.Render(w, r, vd)
+			return
+		}
+		defer file.Close()
+
+		// Create a destination file
+		dst, err := os.Create(filepath.Join(galleryPath, f.Filename))
+		if err != nil {
+			vd.SetAlert(err)
+			g.EditView.Render(w, r, vd)
+			return
+		}
+		defer dst.Close()
+
+		// Copy uploaded file data to the destination file
+		_, err = io.Copy(dst, file)
+		if err != nil {
+			vd.SetAlert(err)
+			g.EditView.Render(w, r, vd)
+			return
+		}
+	}
+
+	vd.Alert = &views.Alert{
+		Level:   views.LevelSuccess,
+		Message: "Images successfully uploaded!",
 	}
 	g.EditView.Render(w, r, vd)
 }
