8833a900c5299ed0ac199cce2463b0d044b3d93b
shengng325 <shengng325@gmail.com>
2020-10-12 15:00:17 +0800

    refactor create image

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -2,10 +2,7 @@ package controllers
 
 import (
 	"fmt"
-	"io"
 	"net/http"
-	"os"
-	"path/filepath"
 	"strconv"
 
 	"github.com/gorilla/mux"
@@ -24,13 +21,14 @@ const (
 )
 
 //init user
-func NewGalleries(gs models.GalleryService, r *mux.Router) *Galleries {
+func NewGalleries(gs models.GalleryService, is models.ImageService, r *mux.Router) *Galleries {
 	return &Galleries{
 		NewGallery: views.NewView("bootstrap", "galleries/new"),
 		ShowView:   views.NewView("bootstrap", "galleries/show"),
 		EditView:   views.NewView("bootstrap", "galleries/edit"),
 		IndexView:  views.NewView("bootstrap", "galleries/index"),
 		gs:         gs,
+		is:         is,
 		r:          r,
 	}
 }
@@ -41,6 +39,7 @@ type Galleries struct {
 	EditView   *views.View
 	IndexView  *views.View
 	gs         models.GalleryService
+	is         models.ImageService
 	r          *mux.Router
 }
 
@@ -142,16 +141,6 @@ func (g *Galleries) ImageUpload(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Create the directory to contain our images
-	galleryPath := filepath.Join("images", "galleries",
-		fmt.Sprintf("%v", gallery.ID))
-	err = os.MkdirAll(galleryPath, 0755)
-	if err != nil {
-		vd.SetAlert(err)
-		g.EditView.Render(w, r, vd)
-		return
-	}
-
 	// Iterate over uploaded files to process them.
 	files := r.MultipartForm.File["images"]
 	for _, f := range files {
@@ -164,17 +153,8 @@ func (g *Galleries) ImageUpload(w http.ResponseWriter, r *http.Request) {
 		}
 		defer file.Close()
 
-		// Create a destination file
-		dst, err := os.Create(filepath.Join(galleryPath, f.Filename))
-		if err != nil {
-			vd.SetAlert(err)
-			g.EditView.Render(w, r, vd)
-			return
-		}
-		defer dst.Close()
-
-		// Copy uploaded file data to the destination file
-		_, err = io.Copy(dst, file)
+		// Create the image
+		err = g.is.Create(gallery.ID, file, f.Filename)
 		if err != nil {
 			vd.SetAlert(err)
 			g.EditView.Render(w, r, vd)
---
+++ models/images.go
@@ -0,0 +1,54 @@
+package models
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+type ImageService interface {
+	Create(galleryID uint, r io.Reader, filename string) error
+	//ByGalleryID(galleryID uint) ([]string, error)
+}
+
+func NewImageService() ImageService {
+	return &imageService{}
+}
+
+type imageService struct{}
+
+func (is *imageService) Create(galleryID uint, r io.Reader, filename string) error {
+	path, err := is.mkImagePath(galleryID)
+	if err != nil {
+		return err
+	}
+	// Create a destination file
+	dst, err := os.Create(filepath.Join(path, filename))
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+	// Copy reader data to the destination file
+	_, err = io.Copy(dst, r)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Going to need this when we know it is already made
+func (is *imageService) imagePath(galleryID uint) string {
+	return filepath.Join("images", "galleries",
+		fmt.Sprintf("%v", galleryID))
+}
+
+// Use the imagePath method we just made
+func (is *imageService) mkImagePath(galleryID uint) (string, error) {
+	galleryPath := is.imagePath(galleryID)
+	err := os.MkdirAll(galleryPath, 0755)
+	if err != nil {
+		return "", err
+	}
+	return galleryPath, nil
+}
