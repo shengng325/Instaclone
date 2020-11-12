d4ebcf20415356d11a20a8d7d109f4f1696fc0c1
shengng325 <shengng325@gmail.com>
2020-11-13 02:06:58 +0800

    added smooth horizontal scrolling

--- assets/styles.js
+++ assets/styles.js
@@ -12,5 +12,39 @@ $( document ).ready(function() {
             btn[1].style.display = "none" 
         }
     });
+
+    getStories = $('.stories')
+    stories = getStories[0]
+    value = 0
+    minValue = 0
+    maxValue = stories.scrollWidth - stories.clientWidth
+    stories.addEventListener('wheel', function(e) {
+        e.preventDefault()
+        if (e.deltaY > 0) {
+            value +=70
+            if (value > maxValue) value = maxValue
+            console.log("cur:", stories.scrollLeft)
+            console.log("target", value)
+            $({someValue: stories.scrollLeft}).animate({someValue: value}, {
+                duration: 200,
+                easing:'linear',
+                step: function() {
+                    stories.scrollLeft = this.someValue;
+                }
+            });
+        } else {
+            value -=70
+            if (value < minValue) value = minValue
+            console.log("cur:", stories.scrollLeft)
+            console.log("target", value)
+            $({someValue: stories.scrollLeft}).animate({someValue: value}, {
+                duration: 200,
+                easing:'linear',
+                step: function() {
+                    stories.scrollLeft = this.someValue;
+                }
+            });
+        }
+    });
 });
 
